18eb0cfca8d1ec44c9ed3c4b19ae071839532d46
quocgiau217 <88599463+quocgiau217@users.noreply.github.com>
2023-03-27 00:09:48 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
+        props: true // Truyền biến trong $route.params vào làm props
     },
     {
         path: "/contacts/create",
