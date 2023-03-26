0d7588da82216d068dd6f98e61dd06c079e8395c
quocgiau217 <88599463+quocgiau217@users.noreply.github.com>
2023-03-26 23:52:26 +0700

    Hien thi danh sach cac contact lay tu server

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+    {
+        path: "/contacts/create",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+})
+
+export default router;
